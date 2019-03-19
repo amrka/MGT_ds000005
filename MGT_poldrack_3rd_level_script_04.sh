69fc55ea1195347d5b4fab6eb73ff8da9dc90c2b
amrka <amr.kamel@uv.es>
2019-03-19 15:04:38 +0100

    Update MGT_poldrack_3rd_level_script_04.sh

--- MGT_poldrack_3rd_level_script_04.sh
+++ MGT_poldrack_3rd_level_script_04.sh
@@ -68,36 +68,8 @@ fslchfiletype NIFTI /Users/amr/Documents/mixed_gambling_poldrack_2007/output_MGT
 #the rest is done locally not on the cluster
 
 
-mkdir /Users/amr/Documents/mixed_gambling_poldrack_2007/output_MGT_poldrack_proc_3rd_level/gain_stat_palm
-mkdir /Users/amr/Documents/mixed_gambling_poldrack_2007/output_MGT_poldrack_proc_3rd_level/loss_stat_palm
-
-
-palm \
--i /Users/amr/Documents/mixed_gambling_poldrack_2007/output_MGT_poldrack_proc_3rd_level/gain_group_cope1.nii \
--o /Users/amr/Documents/mixed_gambling_poldrack_2007/output_MGT_poldrack_proc_3rd_level/gain_stat_palm/gain \
--m /Users/amr/Documents/mixed_gambling_poldrack_2007/output_MGT_poldrack_proc_3rd_level/MNI152_T1_1mm_brain_mask.nii \
--d /Users/amr/Documents/mixed_gambling_poldrack_2007/one_sample_ttest_16sub_MGT_poldrack.mat \
--t /Users/amr/Documents/mixed_gambling_poldrack_2007/one_sample_ttest_16sub_MGT_poldrack.con \
--vg /Users/amr/Documents/mixed_gambling_poldrack_2007/one_sample_ttest_16sub_MGT_poldrack.grp \
--n 5000 -T -C 3.1 -ise -corrcon
-
-
-
-palm \
--i /Users/amr/Documents/mixed_gambling_poldrack_2007/output_MGT_poldrack_proc_3rd_level/loss_group_cope2.nii \
--o /Users/amr/Documents/mixed_gambling_poldrack_2007/output_MGT_poldrack_proc_3rd_level/loss_stat_palm/loss \
--m /Users/amr/Documents/mixed_gambling_poldrack_2007/output_MGT_poldrack_proc_3rd_level/MNI152_T1_1mm_brain_mask.nii \
--d /Users/amr/Documents/mixed_gambling_poldrack_2007/one_sample_ttest_16sub_MGT_poldrack.mat \
--t /Users/amr/Documents/mixed_gambling_poldrack_2007/one_sample_ttest_16sub_MGT_poldrack.con \
--vg /Users/amr/Documents/mixed_gambling_poldrack_2007/one_sample_ttest_16sub_MGT_poldrack.grp \
--n 5000 -T -C 3.1 -ise -corrcon
-
-
-
-
-
-mkdir gain_stat_flameo
-mkdir loss_stat_flameo
+mkdir gain_stat_flameo_positive_and_negative
+mkdir loss_stat_flameo_positive_and_negative
 
 
 
@@ -107,10 +79,10 @@ flameo \
 --cope=/Users/amr/Documents/mixed_gambling_poldrack_2007/output_MGT_poldrack_proc_3rd_level/gain_group_cope1.nii \
 --vc=/Users/amr/Documents/mixed_gambling_poldrack_2007/output_MGT_poldrack_proc_3rd_level/gain_group_varcope1.nii.gz \
 --mask=/Users/amr/Documents/mixed_gambling_poldrack_2007/output_MGT_poldrack_proc_3rd_level/MNI152_T1_1mm_brain_mask.nii \
---ld=/Users/amr/Documents/mixed_gambling_poldrack_2007/output_MGT_poldrack_proc_3rd_level/gain_stat_flameo/ \
---dm=/Users/amr/Documents/mixed_gambling_poldrack_2007/one_sample_ttest_16sub_MGT_poldrack.mat \
---cs=/Users/amr/Documents/mixed_gambling_poldrack_2007/one_sample_ttest_16sub_MGT_poldrack.grp \
---tc=/Users/amr/Documents/mixed_gambling_poldrack_2007/one_sample_ttest_16sub_MGT_poldrack.con \
+--ld=/Users/amr/Documents/mixed_gambling_poldrack_2007/output_MGT_poldrack_proc_3rd_level/gain_stat_flameo_positive_and_negative/ \
+--dm=/Users/amr/Documents/mixed_gambling_poldrack_2007/one_sample_ttest_16sub_MGT_poldrack_positive_and_negative.mat \
+--cs=/Users/amr/Documents/mixed_gambling_poldrack_2007/one_sample_ttest_16sub_MGT_poldrack_positive_and_negative.grp \
+--tc=/Users/amr/Documents/mixed_gambling_poldrack_2007/one_sample_ttest_16sub_MGT_poldrack_positive_and_negative.con \
 --runmode=flame1
 
 
@@ -119,11 +91,12 @@ flameo \
 --cope=/Users/amr/Documents/mixed_gambling_poldrack_2007/output_MGT_poldrack_proc_3rd_level/loss_group_cope2.nii \
 --vc=/Users/amr/Documents/mixed_gambling_poldrack_2007/output_MGT_poldrack_proc_3rd_level/loss_group_varcope2.nii.gz \
 --mask=/Users/amr/Documents/mixed_gambling_poldrack_2007/output_MGT_poldrack_proc_3rd_level/MNI152_T1_1mm_brain_mask.nii \
---ld=/Users/amr/Documents/mixed_gambling_poldrack_2007/output_MGT_poldrack_proc_3rd_level/loss_stat_flameo/ \
---dm=/Users/amr/Documents/mixed_gambling_poldrack_2007/one_sample_ttest_16sub_MGT_poldrack.mat \
---cs=/Users/amr/Documents/mixed_gambling_poldrack_2007/one_sample_ttest_16sub_MGT_poldrack.grp \
---tc=/Users/amr/Documents/mixed_gambling_poldrack_2007/one_sample_ttest_16sub_MGT_poldrack.con \
+--ld=/Users/amr/Documents/mixed_gambling_poldrack_2007/output_MGT_poldrack_proc_3rd_level/loss_stat_flameo_positive_and_negative/ \
+--dm=/Users/amr/Documents/mixed_gambling_poldrack_2007/one_sample_ttest_16sub_MGT_poldrack_positive_and_negative.mat \
+--cs=/Users/amr/Documents/mixed_gambling_poldrack_2007/one_sample_ttest_16sub_MGT_poldrack_positive_and_negative.grp \
+--tc=/Users/amr/Documents/mixed_gambling_poldrack_2007/one_sample_ttest_16sub_MGT_poldrack_positive_and_negative.con \
 --runmode=flame1
+
 
 
 
@@ -136,24 +109,48 @@ mkdir loss_stat_randomise
 
 randomise \
 -i /Users/amr/Documents/mixed_gambling_poldrack_2007/output_MGT_poldrack_proc_3rd_level/gain_group_cope1.nii \
--o /Users/amr/Documents/mixed_gambling_poldrack_2007/output_MGT_poldrack_proc_3rd_level/gain_stat_randomise/gain \
+-o /Users/amr/Documents/mixed_gambling_poldrack_2007/output_MGT_poldrack_proc_3rd_level/gain_stat_randomise/gain_positive_and_negative \
 -m /Users/amr/Documents/mixed_gambling_poldrack_2007/output_MGT_poldrack_proc_3rd_level/MNI152_T1_1mm_brain_mask.nii \
--d /Users/amr/Documents/mixed_gambling_poldrack_2007/one_sample_ttest_16sub_MGT_poldrack.mat \
--t /Users/amr/Documents/mixed_gambling_poldrack_2007/one_sample_ttest_16sub_MGT_poldrack.con \
--e /Users/amr/Documents/mixed_gambling_poldrack_2007/one_sample_ttest_16sub_MGT_poldrack.grp \
+-d /Users/amr/Documents/mixed_gambling_poldrack_2007/one_sample_ttest_16sub_MGT_poldrack_positive_and_negative.mat \
+-t /Users/amr/Documents/mixed_gambling_poldrack_2007/one_sample_ttest_16sub_MGT_poldrack_positive_and_negative.con \
+-e /Users/amr/Documents/mixed_gambling_poldrack_2007/one_sample_ttest_16sub_MGT_poldrack_positive_and_negative.grp \
 -n 1000 -T -c 3.1 --uncorrp --film -x
 
 
 randomise \
 -i /Users/amr/Documents/mixed_gambling_poldrack_2007/output_MGT_poldrack_proc_3rd_level/loss_group_cope2.nii \
--o /Users/amr/Documents/mixed_gambling_poldrack_2007/output_MGT_poldrack_proc_3rd_level/loss_stat_randomise/loss \
+-o /Users/amr/Documents/mixed_gambling_poldrack_2007/output_MGT_poldrack_proc_3rd_level/loss_stat_randomise/loss_positive_and_negative \
 -m /Users/amr/Documents/mixed_gambling_poldrack_2007/output_MGT_poldrack_proc_3rd_level/MNI152_T1_1mm_brain_mask.nii \
--d /Users/amr/Documents/mixed_gambling_poldrack_2007/one_sample_ttest_16sub_MGT_poldrack.mat \
--t /Users/amr/Documents/mixed_gambling_poldrack_2007/one_sample_ttest_16sub_MGT_poldrack.con \
--e /Users/amr/Documents/mixed_gambling_poldrack_2007/one_sample_ttest_16sub_MGT_poldrack.grp \
+-d /Users/amr/Documents/mixed_gambling_poldrack_2007/one_sample_ttest_16sub_MGT_poldrack_positive_and_negative.mat \
+-t /Users/amr/Documents/mixed_gambling_poldrack_2007/one_sample_ttest_16sub_MGT_poldrack_positive_and_negative.con \
+-e /Users/amr/Documents/mixed_gambling_poldrack_2007/one_sample_ttest_16sub_MGT_poldrack_positive_and_negative.grp \
 -n 10000 -T -c 3.1 --uncorrp --film -x
 
 
 
 
+
+mkdir /Users/amr/Documents/mixed_gambling_poldrack_2007/output_MGT_poldrack_proc_3rd_level/gain_stat_palm
+mkdir /Users/amr/Documents/mixed_gambling_poldrack_2007/output_MGT_poldrack_proc_3rd_level/loss_stat_palm
+
+
+palm \
+-i /Users/amr/Documents/mixed_gambling_poldrack_2007/output_MGT_poldrack_proc_3rd_level/gain_group_cope1.nii \
+-o /Users/amr/Documents/mixed_gambling_poldrack_2007/output_MGT_poldrack_proc_3rd_level/gain_stat_palm/gain \
+-m /Users/amr/Documents/mixed_gambling_poldrack_2007/output_MGT_poldrack_proc_3rd_level/MNI152_T1_1mm_brain_mask.nii \
+-d /Users/amr/Documents/mixed_gambling_poldrack_2007/one_sample_ttest_16sub_MGT_poldrack_positive_and_negative.mat \
+-t /Users/amr/Documents/mixed_gambling_poldrack_2007/one_sample_ttest_16sub_MGT_poldrack_positive_and_negative.con \
+-vg /Users/amr/Documents/mixed_gambling_poldrack_2007/one_sample_ttest_16sub_MGT_poldrack_positive_and_negative.grp \
+-n 5000 -T -C 3.1 -ise -corrcon -save1-p
+
+
+
+palm \
+-i /Users/amr/Documents/mixed_gambling_poldrack_2007/output_MGT_poldrack_proc_3rd_level/loss_group_cope2.nii \
+-o /Users/amr/Documents/mixed_gambling_poldrack_2007/output_MGT_poldrack_proc_3rd_level/loss_stat_palm/loss \
+-m /Users/amr/Documents/mixed_gambling_poldrack_2007/output_MGT_poldrack_proc_3rd_level/MNI152_T1_1mm_brain_mask.nii \
+-d /Users/amr/Documents/mixed_gambling_poldrack_2007/one_sample_ttest_16sub_MGT_poldrack_positive_and_negative.mat \
+-t /Users/amr/Documents/mixed_gambling_poldrack_2007/one_sample_ttest_16sub_MGT_poldrack_positive_and_negative.con \
+-vg /Users/amr/Documents/mixed_gambling_poldrack_2007/one_sample_ttest_16sub_MGT_poldrack_positive_and_negative.grp \
+-n 5000 -T -C 3.1 -ise -corrcon -save1-p
 
